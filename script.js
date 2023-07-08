ed56ceb54342d75283589ea6ae2a595361005256
Cody Feldhaus <cody.feldhaus@gmail.com>
2023-07-07 19:19:32 -0700

    adjustments

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ var list = document.getElementById('todo-list');
 
 // Load tasks from local storage
 window.addEventListener('load', function() {
-    for (var i = 0; i < localStorage.length; i++) {
-        addTaskToList(localStorage.key(i));
-    }
+  for (var i = 0; i < localStorage.length; i++) {
+    addTaskToList(localStorage.key(i));
+  }
 });
 
 form.addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var task = input.value;
-    input.value = '';
+  var task = input.value;
+  input.value = '';
 
-    addTaskToList(task);
-    localStorage.setItem(task, task);
+  addTaskToList(task);
+  localStorage.setItem(task, task);
 });
 
 function addTaskToList(task) {
-    var listItem = document.createElement('li');
-    listItem.textContent = task;
-    listItem.addEventListener('click', function() {
-        listItem.remove();
-        localStorage.removeItem(task);
-    });
+  var listItem = document.createElement('li');
+  listItem.textContent = task;
+  listItem.addEventListener('click', function() {
+    listItem.remove();
+    localStorage.removeItem(task);
+  });
 
-    list.appendChild(listItem);
+  list.appendChild(listItem);
 }
